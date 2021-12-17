94b77c175b00ee2701cce07df8a97c9e993e3489
mengxiong10 <15623530290@163.com>
2021-12-17 15:00:15 +0800

    build: add commit hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
